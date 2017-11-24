3baee0ccfbd2eaa61a6d8b1239f1a71a90208097
Colombianito66 <colombianito66@gmail.com>
2017-11-24 17:46:21 -0500

    ad idem

--- src/App.js
+++ src/App.js
@@ -4,31 +4,33 @@ import './App.css';
 
 class App extends Component {
   render() {
-    let i = 1;
-    let myStyle = {
-      fontSize: 20,
-      color: 'red'
-    };
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro" >
-          <h1> What is JSX</h1>
-          <p> in this lecture we will go over the JSX tags </p>
-        </p>
-        <h4 style={myStyle}> HEEEY WORLD!</h4>
-        <p> {1+1}</p>
-        <p>{ i===2 ? 'TRUE' : 'FALSE' }</p>
-        { // YEEE A SINGLE LINE COMMENT
-        }
-        {/* yee 
-        
-        multiline dawg
-        */}
+        <Header />
+        <Content />
       </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+      </div>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <p className="App-intro" >
+      <h1> What is JSX</h1>
+      <p> in this lecture we will go over the JSX tags </p>
+      </p>
     );
   }
 }
